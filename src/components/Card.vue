3973c1b65bc06a2868886b4f034a994afcfec972
ajsmith421 <ajsmith421@gmail.com>
2023-12-15 01:03:51 -0500

    initial Instagram commit

---
+++ src/components/Card.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps(["post"]);
+const { VITE_BASE_PHOTO_URL } = import.meta.env;
+</script>
+
+<template>
+  <ACard hoverable style="width: 240px" class="card">
+    <template #cover>
+      <img alt="example" :src="`${VITE_BASE_PHOTO_URL}${post.url}`" />
+    </template>
+    <ACardMeta :title="post.username">
+      <template #description>{{ post.caption }}</template>
+    </ACardMeta>
+  </ACard>
+</template>
+
+<style scoped>
+.card {
+  margin-bottom: 20px;
+}
+</style>
---
+++ src/components/ImageGallary.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps(["posts"]);
+const { VITE_BASE_PHOTO_URL } = import.meta.env;
+</script>
+
+<template>
+  <div class="image-gallary-container">
+    <img
+      v-for="post in props.posts"
+      :key="post.id"
+      :src="`${VITE_BASE_PHOTO_URL}${post.url}`"
+    />
+  </div>
+</template>
+
+<style scoped>
+.image-gallary-container {
+  display: flex;
+  justify-content: left;
+  flex-wrap: wrap;
+}
+
+img {
+  margin: 5px;
+  width: 200px;
+}
+</style>
